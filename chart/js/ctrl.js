40e015d0ff272b3942ea7cce0c90650525e6c57c
Raí Vitor <raivitor@gmail.com>
2016-10-22 20:18:21 -0300

    pegando dados do web service

--- chart/js/ctrl.js
+++ chart/js/ctrl.js
@@ -1,33 +1,17 @@
 angular.module('myApp', [])
-.controller('MyCtrl', function ($scope) {
-	//$scope.exampleDate = moment().hour(8).minute(0).second(0).toDate();
-    $scope.nome = 'Raí';
-    // numero da mesa
-    var retorno = {
-        "started_desk": 1,
-        "idle_desk": 1,
-        "desks": [
-            {
-                "status": "working",
-                "pk": 1,
-                "finish": null,
-                "order": "131",
-                "start": "16:30:00",
-                "desk_number": 10
+.controller('MyCtrl', function ($scope, $http) {
+    var retorno23;
+	$http({
+        method: 'GET',
+        url: 'http://smartwork-web.herokuapp.com/api/dashboard/'
+        }).then(function successCallback(response) {
+                retorno23 = response.data;
+                $scope.retorno = retorno23;
+                $scope.orders = retorno23.desks;
+                console.log(retorno23.desks);
+          }, function errorCallback(response) {
+            console.error(response);
+    });
 
-            },
-            {
-                "status": "idle",
-                "pk": 2,
-                "finish": "16:30:30",
-                "order": "435",
-                "start": "16:30:10",
-                "desk_number": 5
-            }
-        ],
-        "order_count": 2
-    }
-
-    $scope.retorno = retorno;
-    $scope.orders = retorno["desks"];
+    
 })
