151b5ddef242599b650306f3b20fc23aa0df22a9
Raí Vitor <raivitor@gmail.com>
2016-10-23 04:56:41 -0300

    grafico de espectativa

--- chart/js/ctrl.js
+++ chart/js/ctrl.js
@@ -20,7 +20,7 @@ angular.module('myApp', ['angularMoment'])
         var t = setTimeout(function(){ startTime() }, 200);
     }
 	
-    
+
     function diferenciarDatas(data){
         var velho = moment(data); //data do obj recebido
         var novo = moment(); // data do obj atual
@@ -31,38 +31,111 @@ angular.module('myApp', ['angularMoment'])
 
 
 .controller('CtrlResultados', function ($scope) {
+    var result = {
+        "histories": [
+            49,
+            66,
+            88,
+            135,
+            136,
+            170,
+            210,
+            225,
+            262,
+            295,
+            325,
+            331,
+            370
+        ],
+        "item": "Cade Bray",
+        "deadline": 20,
+        "pk": 4,
+        "daily_goals": [
+            25,
+            50,
+            75,
+            100,
+            125,
+            150,
+            175,
+            200,
+            225,
+            250,
+            275,
+            300,
+            325,
+            350,
+            375,
+            400,
+            425,
+            450,
+            475,
+            500
+        ],
+        "quantity": 500
+    }
     new Chart(document.getElementById("line_chart").getContext("2d"), getChartJs('line'));
 
+    //Criar as labels do gráfico
+    function dividirDias(prazo){
+        var dias = [];
+        for (var i = 0; i < prazo; i++) {
+            dias[i] = "Dia "+(i+1);
+        }
+        return dias;
+    }
+
+    //calcula o mínimo que pode ser feito por dia, para que seja entregue no prazo correto
+    function calcularProducaoMedia(prazo, total) {
+        var prod = [];
+        var prodDia = total/prazo;
+        for (var i = 0; i < prazo; i++) {
+            if(i == 0){
+                prod[i] = prodDia;
+            } else{
+                prod[i] = prod[i-1] + prodDia;
+            }
+        }
+        return prod;
+    }
+
+    // Gera o gráfico
     function getChartJs(type) {
         var config = null;
-
-        if (type === 'line') {
-            config = {
-                type: 'line',
-                data: {
-                    labels: ["Dia 1", "Dia 2", "Dia 3", "Dia 4", "Dia 5", "Dia 6", "Dia 7"],
-                    datasets: [{
-                        label: "Resultado Alcançado",
-                        data: [9, 8, 11, 8, 15, 12, 7],
-                        borderColor: 'rgba(0, 188, 212, 0.75)',
-                        backgroundColor: 'rgba(0, 188, 212, 0.3)',
-                        pointBorderColor: 'rgba(0, 188, 212, 0)',
-                        pointBackgroundColor: 'rgba(0, 188, 212, 0.9)',
-                        pointBorderWidth: 1
-                    }, {
-                        label: "Resultado esperado",
-                        data: [10, 11, 11, 11, 12, 10, 7],
-                        borderColor: 'rgba(233, 30, 99, 0.75)',
-                        backgroundColor: 'rgba(233, 30, 99, 0.3)',
-                        pointBorderColor: 'rgba(233, 30, 99, 0)',
-                        pointBackgroundColor: 'rgba(233, 30, 99, 0.9)',
-                        pointBorderWidth: 1
-                        }]
-                },
-                options: {
-                    responsive: true,
-                    legend: false
-                }
+        config = {
+            type: 'line',
+            data: {
+                labels: dividirDias(result.deadline),
+                datasets: [{
+                    label: "Resultado Esperado",
+                    data: result.daily_goals,
+                    borderColor: 'rgba(0, 188, 212, 0.75)',
+                    backgroundColor: 'rgba(0, 188, 212, 0.3)',
+                    pointBorderColor: 'rgba(0, 188, 212, 0)',
+                    pointBackgroundColor: 'rgba(0, 188, 212, 0.9)',
+                    pointBorderWidth: 1
+                }, {
+                    label: "Resultado Alcançado",
+                    data: result.histories,
+                    borderColor: 'rgba(233, 30, 99, 0.75)',
+                    backgroundColor: 'rgba(233, 30, 99, 0.3)',
+                    pointBorderColor: 'rgba(233, 30, 99, 0)',
+                    pointBackgroundColor: 'rgba(233, 30, 99, 0.9)',
+                    pointBorderWidth: 1
+                    },
+                    {
+                    label: "Resultado Médio",
+                    data: calcularProducaoMedia(result.deadline, result.quantity),
+                    borderColor: 'rgba(50, 50, 50, 0.75)',
+                    backgroundColor: 'rgba(50, 50, 50, 0.3)',
+                    pointBorderColor: 'rgba(50, 50, 50, 0)',
+                    pointBackgroundColor: 'rgba(50, 50, 50, 0.9)',
+                    pointBorderWidth: 1
+                    }]
+            },
+            options: {
+                responsive: true,
+                legend: false
             }
         }
         return config;
